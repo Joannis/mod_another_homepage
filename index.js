e94989615e12ccf49f57e6fb7b547fd0f416724f
joannis <joannis@ONION>
2014-07-19 17:13:11 +0200

    - Removed useless code

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-/*
- * Breach: [mod_test] index.js
- *
- * Copyright (c) 2014, Stanislas Polu. All rights reserved.
- *
- * @author: spolu
- *
- * @log:
- * - 2014-06-26 spolu   Creation
- */
-'use strict';
+"use strict"
 
+var express = require('express');
+var http = require('http');
+//var async = require('async');
 var breach = require('breach_module');
 
-breach.init(function() {
-  breach.expose('init', function(src, args, cb_) {
-    console.log('Initialization');
-    return cb_();
-  });
+var bootstrap = function(port) {
+  breach.init(function(cb_) {
+    breach.register('core', 'inst:.*');
 
-  breach.expose('kill', function(args, cb_) {
-    common.exit(0);
-  });
-});
+    breach.expose('init', function(src, args, cb_) {
+      breach.module('core').call('tabs_new_tab_url', {
+        url: 'http://127.0.0.1:' + port + '/newtab'
+      }, function(err) {
+        console.log('New tab page set! [' + err + ']');
+      });
+      return cb_();
+    });
 
-process.on('uncaughtException', function (err) {
-  common.fatal(err);
-});
+    breach.expose('kill', function(args, cb_) {
+      process.exit(0);
+    });
+
+    console.log('Exposed: `http://127.0.0.1:' + port + '/newtab`');
+  });
+};
+
+(function setup()
+{
+  var page = express();
+  
+  page.use('/', express.static(__dirname + '/home'));
+  page.use(require('body-parser')());
+  page.use(require('method-override')())
+  
+  var http_srv = http.createServer(page).listen(0, '127.0.0.1');
+  
+  http_srv.on('listening', function() {
+    return bootstrap(http_srv.address().port);
+  });
+  
+})();
